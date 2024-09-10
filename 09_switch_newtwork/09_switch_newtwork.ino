09f2e333aeae8321a70319de1356cb5161127c30
Byungjun Kwon <byungjun@gmail.com>
2024-09-10 17:17:52 +0900

    Create 09_switch_newtwork.ino

---
+++ 09_switch_newtwork/09_switch_newtwork.ino
@@ -0,0 +1,26 @@
+/*
+
+  https://seoul-open-media.slack.com/archives/C0747PEVCHJ/p1724459026978029
+
+*/
+
+int pushButton1 = 23; // L
+int pushButton2 = 29; //R
+
+void setup() {
+
+  Serial.begin(9600);
+
+  pinMode(pushButton1, INPUT);
+  pinMode(pushButton2, INPUT);
+}
+
+
+void loop() {
+  // read the input pin:
+  int buttonState1 = digitalRead(pushButton1);
+  int buttonState2 = digitalRead(pushButton2);
+  // print out the state of the button:
+  Serial.print(buttonState1);  Serial.print(", "); Serial.println(buttonState2);
+  delay(100);   
+}
