a888af9663050f4c0e434804c0e5b279835a0e7d
Byungjun Kwon <byungjun@gmail.com>
2024-09-27 11:19:15 +0900

    update

---
+++ 11_Ahae_test01/EBIMU.ino
@@ -0,0 +1,36 @@
+int EBimuAsciiParser(float *item, int number_of_item)
+{
+  int n,i;
+  int rbytes;
+  char *addr; 
+  int result = 0;
+  
+  rbytes = IMU_SERIAL.available();
+ // Serial.print("rbytes:");  Serial.println(rbytes); 
+  for(n=0;n<rbytes;n++)
+  {
+    sbuf[sbuf_cnt] = IMU_SERIAL.read();
+    if(sbuf[sbuf_cnt]==0x0a)
+       {
+           addr = strtok(sbuf,",");
+           for(i=0;i<number_of_item;i++)
+           {
+              item[i] = atof(addr);
+              addr = strtok(NULL,",");
+           }
+
+           result = 1;
+
+         // IMU_SERIAL.print("\n\r");
+         // for(i=0;i<number_of_item;i++)  {  IMU_SERIAL.print(item[i]);  Serial.print(" "); }
+       }
+     else if(sbuf[sbuf_cnt]=='*')
+       {   sbuf_cnt=-1;
+       }
+
+     sbuf_cnt++;
+     if(sbuf_cnt>=SBUF_SIZE) sbuf_cnt=0;
+  }
+  
+  return result;
+}
